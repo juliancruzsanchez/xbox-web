58c15026f03e5742136e788a70bcf3beab753d4a
juliancruzsanchez <kickbutt.js@gmail.com>
2021-04-15 12:34:02 -0600

    Localized

---
+++ api/Xbox/index.js
@@ -0,0 +1,7 @@
+const xbox = require("./Xbox/index.js");
+const token = require("./Xbox/token.js");
+
+module.exports = {
+  Account: xbox,
+  Token: token
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const xls = require("app-xbox-live");
-
+const xls = require("./api");
+let token,xl;
 Array.prototype.filterKeys = function (keys = []) {
   let retArray = []
   this.forEach(x => {
@@ -11,15 +11,16 @@ Array.prototype.filterKeys = function (keys = []) {
   })
  return retArray
 }
+xls.Token("kickbutt.js@gmail.com", "00677949Aa!").then(x => {token = x; xl = new xls.Account(`XBL3.0 x=${token[1]};${token[0]}`);goTime()})
 
-async function run(params) {
-const token = await xls.Token("kickbutt.js@gmail.com", "00677949Aa!");
-const xl = new xls.Account(`XBL3.0 x=${token[1]};${token[0]}`);
-xl.chat.inbox.get().then(console.log);
-// xl.chat.message.get("2535460039058026", 100).then(console.log);
-
+async function getAllMessages(xuid) {
+return await xl.chat.message.get(xuid, 100)
+}
+async function goTime(params) {
+ // console.log(token)
 const friends = (await xl.me.profile.friends.get()).people
 const xuidAssociations = friends.filterKeys(['xuid', 'displayName', 'displayPicRaw','presenceState','presenceDetails','isFavorite'])
+xl.chat.inbox.get().then(console.log);
+xl.chat.message.get("2535460039058026", 100)
 }
-run()
 
